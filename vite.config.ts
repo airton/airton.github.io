d5b973faf9a83e715e2784ab417917547b61cec5
Airton Vancin Junior <airtonvancin@gmail.com>
2025-12-09 15:15:32 -0300

    fix: configure path aliases for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 import remarkGfm from 'remark-gfm'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mdx({ remarkPlugins: [remarkGfm] })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
+
