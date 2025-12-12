fd5cc283d46104c79706a350a182b91578d3119c
Airton Vancin Junior <airtonvancin@gmail.com>
2025-12-12 12:07:46 -0300

    better page zero downtime and cosmetic changes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,12 +7,18 @@ export function Navigation() {
     <Menubar className="rounded-full border-foreground shadow-lg gap-4 opacity-95 bg-white dark:bg-gray-900">
       <UserAvatar />
       <MenubarMenu>
-        <MenubarTrigger className="rounded-full" asChild>
+        <MenubarTrigger
+          className="rounded-full cursor-pointer hover:bg-gray-100"
+          asChild
+        >
           <a href="/blog">Blog</a>
         </MenubarTrigger>
       </MenubarMenu>
       <MenubarMenu>
-        <MenubarTrigger className="rounded-full" asChild>
+        <MenubarTrigger
+          className="rounded-full cursor-pointer hover:bg-gray-100"
+          asChild
+        >
           <a href="/zero-downtime" className="flex items-center gap-1">
             <IconWashDrycleanOff className="h-4 w-4" /> Zero Downtime
           </a>
--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,15 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
 export function UserAvatar() {
   return (
-    <Avatar className="w-[30px] h-[30px]">
-      <a href="/">
-        <AvatarImage
-          src="https://github.com/airtonvancin.png"
-          className="p-0 m-0 "
-        />
+    <div>
+      <a
+        href="/"
+        className="w-[30px] h-[30px] rounded-full block overflow-hidden"
+      >
+        <img src="https://github.com/airton.png" className="p-0 m-0 " />
       </a>
-      <AvatarFallback>AV</AvatarFallback>
-    </Avatar>
+    </div>
   );
 }
