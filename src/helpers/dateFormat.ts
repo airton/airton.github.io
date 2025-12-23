3021dea472761dc2c3c11dd9500b19592f3f0eae
Airton Vancin Junior <airtonvancin@gmail.com>
2025-12-23 16:55:35 -0300

    chore: fix format date error

--- src/helpers/dateFormat.ts
+++ src/helpers/dateFormat.ts
@@ -1,21 +1,18 @@
-import { splitDate } from "./splitDate";
-
 // Date Format
 export const dateFormat = (value: string | Date) => {
-  let date: Date;
-  if (typeof value === "string") {
-    const objDate = splitDate(value);
-    const UTCDate = `${objDate.valMonth}/${objDate.valDay}/${objDate.valYear}`;
-    date = new Date(UTCDate);
-  } else {
-    date = value;
-  }
+  // If the value is a string 'YYYY-MM-DD', new Date() will parse it as UTC.
+  // If it's a Date object, we'll use it as is.
+  const date = typeof value === "string" ? new Date(value) : value;
 
-  const options = { year: "numeric", month: "short", day: "numeric" };
-  const dateFormated = date.toLocaleDateString(
-    "pt-BR",
-    options as Record<string, string>
-  );
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'UTC', // Treat the date as UTC and format it as UTC
+  };
+  
+  const dateFormated = date.toLocaleDateString('pt-BR', options);
 
   return dateFormated;
 };
+
